9dedc9ba7a7bb790990ca611ab930f938d2b5734
caoyingjunz <cao.yingjunz@gmail.com>
2026-07-03 10:41:23 +0800

    Bump pixiuctl version to 0.2.4

--- pkg/pixiuctl/cmd/version.go
+++ pkg/pixiuctl/cmd/version.go
@@ -8,7 +8,7 @@ import (
 
 const (
 	// Version is the pixiuctl client version.
-	Version = "pixiuctl version 0.2.3"
+	Version = "pixiuctl version 0.2.4"
 )
 
 // NewVersionCommand prints the pixiuctl version.
